da067c739a2f58e9b0cdabf0be8b6c625f91ef0f
Yaret Berges Calero <yaretejea@gmail.com>
2026-06-04 01:55:16 +0200

    Editing ecr config to use IMMUTABLE

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,7 +2,7 @@ module "ecr" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-ecr.git?ref=v3.2.0"
 
   repository_name                 = "${var.project_name}-ecr-${terraform.workspace}/${var.project_name}"
-  repository_image_tag_mutability = "MUTABLE"
+  repository_image_tag_mutability = "IMMUTABLE"
   repository_image_scan_on_push   = true
   repository_encryption_type      = "KMS"
   repository_kms_key              = data.aws_kms_alias.ecr.target_key_arn
