22fbe622dc26b70e172fce06f604e2184bf14a00
Yaret Berges Calero <yaretejea@gmail.com>
2026-08-04 18:08:26 +0200

    Temp push: Forces the creation of the CICD role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -53,7 +53,7 @@ module "github_actions_role" {
     }
     lambda = {
       actions   = ["lambda:UpdateFunctionCode", "lambda:GetFunction"]
-      resources = [module.lambda.lambda_function_arn_static]
+      resources = ["*"]
     }
     s3 = {
       actions = ["s3:PutObject", "s3:GetObject", "s3:DeleteObject", "s3:ListBucket"]
